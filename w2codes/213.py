3fa98c3e5043a7d4673550cc56ecc515376b61be
Akniyet <aknietnroe@gmail.com>
2026-02-07 00:33:28 +0500

    Create 213.py

---
+++ w2codes/213.py
@@ -0,0 +1,18 @@
+n = int(input())
+def isprime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    i = 3
+    while i * i <= n:
+        if n % i == 0:
+            return False
+        i += 2
+    return True
+if isprime(n):
+    print("Yes")
+else:
+    print("No")
